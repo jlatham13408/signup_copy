c9b6e75acfa96532fdb22eb0538e67db9798ff7d
Jonnel Latham <Jonnel Latham>
2016-01-29 20:22:27 -0500

    added the logs

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require('express'),
     server = express();
 
-server.use(express.static(__dirname+’/css’));
+server.use(express.static(__dirname+'/css'));
 server.get('/', function(req, res){
 	res.sendFile('html/index.html', { root: __dirname });
 });
 
-server.post(‘/signup’, function(req. res){
-console.log(“You Signed up!”);
+server.post('/signup', function(req, res){
+console.log("You Signed up!");
 console.log(req.body); // You do not have to put this.  It may confuse you
-res.redirect(‘/’);
+res.redirect('/');
 });
 
 server.listen(8080, function(){
