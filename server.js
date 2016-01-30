586a9ce55878e138d630d2cc4dbd7dc0072c0404
Jonnel Latham <Jonnel Latham>
2016-01-29 20:06:36 -0500

    Initial Commit

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express'),
     server = express();
 
+server.use(express.static(__dirname+’/css’));
 server.get('/', function(req, res){
 	res.sendFile('html/index.html', { root: __dirname });
+});
+
+server.post(‘/signup’, function(req. res){
+console.log(“You Signed up!”);
+console.log(req.body); // You do not have to put this.  It may confuse you
+res.redirect(‘/’);
 });
 
 server.listen(8080, function(){
